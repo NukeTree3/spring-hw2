5cca8d96bbeb5518f3aedd51f25c1884e7815f2d
NukeTree3 <nuketree3@gmail.com>
2025-04-08 22:28:29 +0300

    Initial commit

---
+++ src/test/java/com/nuketree3/example/springhw1/SpringHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuketree3.example.springhw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
